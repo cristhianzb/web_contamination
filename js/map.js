baeadd81d6ef80a8331876a5598bfa532d4ecfa1
rodrigo <salguero_ro@hotmail.com>
2017-11-27 00:01:52 -0300

    muestra latitud longitud, parsea archivo csv

--- js/map.js
+++ js/map.js
@@ -1,5 +1,7 @@
 
 var user=false;
+var latitud;
+var longitud;
 function setUser(active){
   user=active;
 }
@@ -9,7 +11,6 @@ function initMap(){
         zoom:12,
         center:{lat:-17.413977,lng:-66.165321}
       }
-      var subir= "<form action="+'"'+"procesar.php"+'"'+" method="+'"'+'"'+"> <div id="+'"'+"div_file"+'"'+"> <p id="+'"'+"texto"+'"'+"> Add file <input type="+'"'+"file"+'"'+" name="+'"'+"file"+'"'+" id="+'"'+"csv"+'"'+"> </div></form>"
       //New map
       var map = new google.maps.Map(document.getElementById('map'),options);
 
@@ -17,7 +18,7 @@ function initMap(){
       google.maps.event.addListener(map, 'click', function(event){
         //Add marker
         if(user)
-          addMarker({coords:event.latLng,content: subir});
+          addMarker({coords:event.latLng,content:"<h2>asd</h2>"});
         
       });
       //Array of markers
@@ -25,7 +26,6 @@ function initMap(){
         {
           coords:{lat:-17.413977,lng:-66.165321},
           iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-          content: ' chart.draw(data, options);'
         },
         {
           coords:{lat:-17.401977,lng:-66.135000},
@@ -49,7 +49,13 @@ function initMap(){
           map:map,
           //icon:props.iconImage
         });
-
+        marker.addListener('click',function(){
+            var pos = marker.position.toString().split(",");
+            latitud = pos[0].substring(1,pos[0].length-1);
+            longitud = pos[1].substring(1,pos[1].length-1);
+            console.log(latitud+" "+longitud);
+            actualizar();
+        });
         //Check for custom icon
         if(props.iconImage){
           // Set icon image
